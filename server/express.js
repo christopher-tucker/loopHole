16c3274278ae6c7b0f92927a2394c02995878a2e
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-25 12:52:51 -0700

    static html rendering to page

--- server/express.js
+++ server/express.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static(__dirname + '/../client/public'));
 
 app.get('/', (req, res) => res.send('loopHole'));
 
