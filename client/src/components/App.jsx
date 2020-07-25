8335fe946b82d1fd2e1fcf68fdfe36f0e0169085
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-25 13:07:32 -0700

    webpack configured. app component rendering to the page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    const {  } = this.state;
+    return (
+        <div className="AppComponentDiv">
+          App component main div
+        </div>
+    );
+  }
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,49 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: `${SRC_DIR}/index.js`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: path.resolve(DIST_DIR)
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.(js|jsx|css)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        loader: ['style-loader', 'css-loader']
       }
     ]
   }
 };
+
+
+// module.exports = {
+//   entry: `${SRC_DIR}/index.jsx`,
+//   output: {
+//     filename: 'bundle.js',
+//     path: DIST_DIR
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?/,
+//         include: SRC_DIR,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       }
+//     ]
+//   }
+// };
